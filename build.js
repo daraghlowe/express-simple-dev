2b2fccd06cd435371f9816cb759da48902165cca
Daragh Lowe <daragh.lowe@wpengine.com>
2022-04-28 14:45:59 +0100

    adding a sleep for testing

--- build.js
+++ build.js
@@ -1,2 +1,12 @@
 console.log('MY_SECRET: ', process.env.MY_SECRET);
 console.log('ALL ENVS: ', process.env);
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+(async function() {
+  console.log('Waiting 500 seconds');
+  await sleep(50000);
+  console.log('Done waiting!')
+})();
