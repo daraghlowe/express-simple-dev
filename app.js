8ef62eb1eb2d4e932a2667f5a96077a5c29f2875
Daragh Lowe <daragh.lowe@wpengine.com>
2025-02-24 15:00:33 +0000

    change

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
+const os = require('os');
 
 // Add a new router for each test case
 app.use('', require('./routes/base'));
@@ -12,6 +13,8 @@ app.use('/_next/image', require('./routes/next-image'));
 const server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
   console.log(`Nginx listening at http://localhost:8081`)
+  console.log("The network interfaces which have been assigned to this Network address are: ")
+  console.log(os.networkInterfaces());
 })
 
 // Handle Sigterm for quicker shutdowns in Docker
